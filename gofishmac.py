f4e14be6e44f7f60c514e99c6eb6d314ca0a51da
jersmo <36084032+jersmo@users.noreply.github.com>
2023-09-28 19:35:21 -0400

    newish

--- gofishmac.py
+++ gofishmac.py
@@ -7,24 +7,31 @@ You may use, distribute and modify this code.  You may not resell or profit from
 import pyautogui
 import time
 import random
+from mss.darwin import MSS as mss
+import mss.tools as tools
+import PIL as pil
+from PIL import ImageCms
+import io
 #from mousekey import MouseKey  # https://pypi.org/project/mousekey/
 
 
-class GoFish:
+class GoFishMac:
     """Fishing Simulator Bot for Roblox"""
+
 
     @classmethod
     def start(cls):
         # SET THESE THINGS BEFORE YOU RUN THE SCRIPT
+        # TODO: MAKE SURE MAC COLOR PROFILE IS SET TO sRGB AND TRUE TONE IS OFF FOR THIS TO WORK
+        # TODO: MAKE SURE MAC SCREEN SIZE IS SET TO 1680X1050 YOU DO THIS IN DISPLAY AS WELL WITH THE PRESETS
 
         # If you just want to open chests set True
         OPEN_CHESTS = False  # To open chests only.  Try to get E over the back of your neck in game while at chest.
 
         #  For fishing set OPEN_CHESTS to False and set these parameters
-        CATCH_COUNT = 90  # Sell after you catch how many fish?  Backpack size minus slots used with equipment
-        FISHING_DELAY = .25  # .25 for main, .41 for perfect balance and total noob - Lower is faster
-        FISHING_RANDOM = .2  # Random delay to add to casts to make less bannable, .2 or .1 for mains .05 for total noob.
-        FISHING_POLE_SLOT = 1  # What slot is fishing pole in?
+        CATCH_COUNT = 195  # Sell after you catch how many fish?  Backpack size minus slots used with equipment
+        FISHING_DELAY = .05  # .25 for main, .41 for perfect balance and total noob - Lower is faster
+        FISHING_RANDOM = .15  # Random delay to add to casts to make less bannable, .2 or .1 for mains .05 for total noob.
         TIME_FOR_BUBBLES = 12  # Max time to wait after casting to detect bubbles.  Noobs might need 5 or 6
         SELL_ANYWHERE = True  # If you purchased sell anywhere.  Otherwise the script will exit.
 
@@ -38,13 +45,37 @@ class GoFish:
         # # Kills the whole process
         # mkey.enable_failsafekill('ctrl+c')
 
+        cls.sct = mss()
+
         time.sleep(3)
-        print('got past sleep')
 
         # Function for human like mouse clicking
         def pyclick(x, y):
             """Uses pyautogui to move and click mouse"""
             pyautogui.click(x, y)
+
+        def screenshot(counter):
+            # The monitor or screen part to capture
+            monitor = cls.sct.monitors[1]  # or a region
+            # Grab the data
+            sct_img = cls.sct.grab(monitor)
+            # Generate the PNG
+            img = convert_to_srgb(pil.Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX"))
+            # resize to 1680 x 1050
+            img = img.resize((1680, 1050))
+            # output = f"monitor-{counter}.png"
+            # img.save(output)
+
+            return img
+        def convert_to_srgb(img):
+            '''Convert PIL image to sRGB color space (if possible)'''
+            icc = img.info.get('icc_profile', '')
+            if icc:
+                io_handle = io.BytesIO(icc)  # virtual file
+                src_profile = ImageCms.ImageCmsProfile(io_handle)
+                dst_profile = ImageCms.createProfile('sRGB')
+                img = ImageCms.profileToProfile(img, src_profile, dst_profile)
+            return img
 
         def mclick(x, y):
             try:
@@ -56,32 +87,29 @@ class GoFish:
         def mmove(x, y):
             duration = random.uniform(.5, 1)
             try:
-                pyautogui.moveTo(x, y, duration=2, tween=pyautogui.easeInOutQuad)
+                pyautogui.moveTo(x, y, duration=.2, tween=pyautogui.easeInOutQuad)
             except:
-                pyautogui.moveTo(x, y, duration=2, tween=pyautogui.easeInOutQuad)
+                pyautogui.moveTo(x, y, duration=.2, tween=pyautogui.easeInOutQuad)
 
         # Function to check for air bubbles on the screen
-        def check_air_bubbles():
-            print('start: {}'.format(time.time()))
-            s = pyautogui.screenshot()
-            print('stop: {}'.format(time.time()))
-            color = (69, 252, 234)
-            print('startloop: {}'.format(time.time()))
-            for x in range(540, 1160, 5):
-                for y in range(125, 540, 5):
+        def check_air_bubbles(counter):
+            s = screenshot(counter)
+            color = (68, 252, 234)
+            for x in range(600, 1100, 5):
+                for y in range(100, 500, 5):
                     if s.getpixel((x, y)) == color:
                         print('found air bubbles')
                         return True
-            print('stop loop, NO air bubbles {}'.format(time.time()))
             return False
 
         # Function to wait for air bubbles on the screen
         def wait_for_air_bubbles(timeout):
             """Waits for air bubbles"""
             now = time.time()
+            counter =0
             while time.time() - now < timeout:
-                print('run air bubbles')
-                if check_air_bubbles():
+                counter += 1
+                if check_air_bubbles(counter):
                     return True
             return False
 
@@ -105,13 +133,12 @@ class GoFish:
         # Checks if fish on the line
         def fish_on():
             """Detects if the fishing bar is up"""
-            s = pyautogui.screenshot()
+            s = screenshot(0)
             color = (83, 250, 83)  # Green Catch Bar
             # Step every 5 pixels to save time
             for x in range(699, 966, 5):
                 for y in range(702, 780, 5):
                     if s.getpixel((x, y)) == color:
-                        print('fishon')
                         return True
             # Return False at end of the loop
             return False
@@ -130,11 +157,10 @@ class GoFish:
         # Function to reel in the fish
         def reel():
             x, y = random.randint(830, 835), random.randint(490, 497)
-            mmove(x, y)
+            #mmove(x, y)
             wait_for_fish_on(5)
             while fish_on():
                 mclick(x, y)
-                print('clicking')
                 time.sleep(random.uniform(FISHING_DELAY, FISHING_DELAY + FISHING_RANDOM))
             cls.fish_count += 1
             cls.total_fish += 1
@@ -145,10 +171,14 @@ class GoFish:
         # Function to check if fishing pole is selected and select if not
         def is_pole_selected():
             # If fishing pole is not selected, select it
-            x1 = 600 + (85 * (FISHING_POLE_SLOT-1))
-            if not pyautogui.pixelMatchesColor(x1, 875, (0, 213, 255)):
-                x2, y2 = 693 + (85 * (FISHING_POLE_SLOT-1)), random.randint(964, 974)
-                mclick(x2, y2)
+            s = screenshot(0)
+            color = (0, 213, 255)  # Green Catch Bar
+            # Step every 5 pixels to save time
+            for x in range(550, 570, 1):
+                for y in range(870, 890, 1):
+                    if s.getpixel((x, y)) == color:
+                        return
+            mclick(600, 911)
 
         # Function to check the fish count
         def check_fish_count():
@@ -168,34 +198,33 @@ class GoFish:
             time.sleep(random.uniform(.25, .5))
 
             # Click backpack
-            mclick(random.randint(1180, 1230), random.randint(936, 997))
+            mclick(random.randint(1058, 1065), random.randint(915, 925))
             # Wait for green sell box
-            wait_for_color(1100, 350, (84, 234, 52), 2)
+            wait_for_color(977, 353, (84, 234, 52), 2)
 
             # Make sure backpack is selected
-            if not pyautogui.pixelMatchesColor(698, 346, (0, 213, 255)):
-                mclick(698, 346)
+            if not pyautogui.pixelMatchesColor(600, 345, (72, 242, 255)):
+                mclick(600, 345)
 
             #  If there are fish in backpack, sell
-            if pyautogui.pixel(579, 495) != (249, 248, 248):
+            if pyautogui.pixel(506, 461) != (249, 248, 248):
                 # click sell button
-                x, y = random.randint(1037, 1097), random.randint(335, 375)
+                x, y = random.randint(910, 980), random.randint(340, 350)
                 mclick(x, y)
-                wait_for_color(1193, 444, (84, 234, 52), 2)
+                wait_for_color(1056, 406, (119, 255, 100), 2)
 
                 # click sell all
-                x, y = random.randint(1174, 1320), random.randint(416, 452)
+                x, y = random.randint(1050, 1150), random.randint(410, 430)
                 mclick(x, y)
-                wait_for_color(1122, 444, (84, 234, 52), 2)
+                wait_for_color(1055, 411, (119, 255, 100), 2)
 
                 # click final sell button
-                x, y = random.randint(1140, 1155), random.randint(430, 440)
+                x, y = random.randint(995, 1045), random.randint(410, 430)
                 mclick(x, y)
-                wait_for_color(1122, 444, (255, 255, 255), 2)
+                wait_for_color(995, 1045, (255, 255, 255), 2)
 
             # Close backpack
-            x, y = random.randint(1397, 1400), random.randint(363, 366)
-            mclick(x, y)
+            mclick(random.randint(1058, 1065), random.randint(915, 925))
 
             time.sleep(random.uniform(.25, .5))
             # Cast
@@ -203,11 +232,11 @@ class GoFish:
 
         def is_bag_full():
             """Detects if the bag full message is up"""
-            s = pyautogui.screenshot()
+            s = screenshot(0)
             color = (253, 0, 97)  # "Cannot Catch Fish Because Backpack is Full" message color
             # Step every 2 pixels to save time
-            for x in range(570, 670, 2):
-                for y in range(650, 750, 2):
+            for x in range(500, 650, 2):
+                for y in range(645, 750, 2):
                     if s.getpixel((x, y)) == color:
                         return True
             return False
@@ -228,8 +257,8 @@ class GoFish:
             while True:
                 if chest_ready():
                     # Hold E
-                    mkey.force_activate_window(10290540)
-                    mkey.press_key('e', delay=2)  # delay in seconds
+                    #mkey.force_activate_window(10290540)
+                    #mkey.press_key('e', delay=2)  # delay in seconds
                     # Click buy
                     mclick(697, 831)
                     time.sleep(6)
@@ -244,10 +273,8 @@ class GoFish:
         time.sleep(.5)
         is_pole_selected()
         while True:
-            print('got to main loop, getting ready to cast')
             # Cast
             double_click_random_cast()
-            print('casted')
 
             # Check for full inventory or sell fish if needed
             if check_fish_count():
@@ -261,4 +288,4 @@ class GoFish:
 
 
 if __name__ == '__main__':
-    GoFish.start()
+    GoFishMac.start()
