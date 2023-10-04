16bb0889800987a7f2f0e3355de0747c5424fd24
jersmo <36084032+jersmo@users.noreply.github.com>
2023-10-04 12:26:29 -0400

    Create fish.py

---
+++ fish.py
@@ -0,0 +1,416 @@
+""""
+Safe Fish -
+Copyright (C) 2023 JSmo - All Rights Reserved.
+You may use, distribute and modify this code.  You may not resell or profit from it.
+"""
+
+import os
+import pyautogui
+import time
+import random
+if os.name == "nt":
+    #  Windows imports
+    import win32api
+    import win32con
+    from mousekey import MouseKey  # https://pypi.org/project/mousekey/
+elif os.name == "posix":
+    if os.uname()[0] == "Darwin":
+        # import mac_modules
+        from mss.darwin import MSS as mss
+        import PIL as pil
+        from PIL import ImageCms
+        import io
+    else:
+        # No linux support
+        exit()
+
+
+class Fish:
+    """Roblox robot common functions."""
+
+    @classmethod
+    def main(cls):
+        #  Set the proper helper class based on operating system
+        cls.windows = True if os.name == 'nt' else False
+        cls.m = None
+        if cls.windows:
+            cls.m = WindowsHelper()
+        else:
+            cls.m = MacHelper()
+        m = cls.m
+
+        # SET THESE THINGS BEFORE YOU RUN THE SCRIPT
+        # TODO: MAKE SURE MAC COLOR PROFILE IS SET TO sRGB AND TRUE TONE IS OFF FOR THIS TO WORK
+        # TODO: MAKE SURE MAC SCREEN SIZE IS SET TO 1680X1050 YOU DO THIS IN DISPLAY AS WELL WITH THE PRESETS
+
+        # If you just want to open chests set True
+        OPEN_CHESTS = False  # To open chests only.  Try to get E over the back of your neck in game while at chest.
+
+        #  For fishing set OPEN_CHESTS to False and set these parameters
+        CATCH_COUNT = 195  # Sell after you catch how many fish?  Backpack size minus slots used with equipment
+        FISHING_DELAY = .05  # .25 for main, .41 for perfect balance and total noob - Lower is faster
+        FISHING_RANDOM = .15  # Random delay to add to casts to make less bannable, .2 or .1 for mains .05 for total noob.
+        TIME_FOR_BUBBLES = 12  # Max time to wait after casting to detect bubbles.  Noobs might need 5 or 6
+        SELL_ANYWHERE = True  # If you purchased sell anywhere.  Otherwise the script will exit.
+
+        #  Class Variables
+        cls.total_fish = 0
+        cls.fish_count = 0
+        cls.start_time = time.time()
+
+        # Delay before clicking into roblox
+        time.sleep(1)
+
+        def check_air_bubbles(counter):
+            s = m.screenshot(counter)
+            color = (68, 252, 234)
+            for x in range(600, 1100, 5):
+                for y in range(100, 500, 5):
+                    if s.getpixel((x, y)) == color:
+                        print('found air bubbles')
+                        return True
+            return False
+
+        def wait_for_air_bubbles(timeout):
+            """Waits for air bubbles"""
+            now = time.time()
+            counter = 0
+            while time.time() - now < timeout:
+                counter += 1
+                if check_air_bubbles(counter):
+                    return True
+            return False
+
+        def wait_for_fish_on(timeout):
+            """Waits for fishing bar up"""
+            now = time.time()
+            while time.time() - now < timeout:
+                if fish_on():
+                    return
+
+        # Checks if fish on the line
+        def fish_on():
+            """Detects if the fishing bar is up"""
+            s = m.screenshot(0)
+            color = (83, 250, 83)  # Green Catch Bar
+            # Step every 5 pixels to save time
+            for x in range(699, 966, 5):
+                for y in range(702, 780, 5):
+                    if s.getpixel((x, y)) == color:
+                        return True
+            # Return False at end of the loop
+            return False
+
+        def click_random_cast():
+            x, y = random.randint(830, 835), random.randint(490, 497)
+            m.click(x, y)
+
+        # Function to simulate a double random cast click
+        def double_click_random_cast():
+            click_random_cast()
+            time.sleep(random.uniform(0.02, 0.025))
+            click_random_cast()
+
+        def reel():
+            x, y = random.randint(830, 835), random.randint(490, 497)
+            wait_for_fish_on(5)
+            while fish_on():
+                m.click(x, y)
+                time.sleep(random.uniform(FISHING_DELAY, FISHING_DELAY + FISHING_RANDOM))
+            cls.fish_count += 1
+            cls.total_fish += 1
+            print('Fish caught: {}/{}.  Total fish caught this session: {}.  Total session time: {}'.format(
+                cls.fish_count, CATCH_COUNT, cls.total_fish,
+                time.strftime("%Hh:%Mm:%Ss", time.gmtime(time.time()-cls.start_time))))
+
+        # Function to check if fishing pole is selected and select if not
+        def is_pole_selected():
+            # If fishing pole is not selected, select it
+            s = m.screenshot(0)
+            color = (0, 213, 255)  # Green Catch Bar
+            # Step every 5 pixels to save time
+            for x in range(550, 570, 1):
+                for y in range(870, 890, 1):
+                    if s.getpixel((x, y)) == color:
+                        return
+            m.click(600, 911)
+
+        def check_fish_count():
+            if cls.fish_count == CATCH_COUNT or is_bag_full():
+                if SELL_ANYWHERE:
+                    print('Inventory full, selling...')
+                    sell_fish()
+                else:
+                    print('Inventory full, exiting program')
+                    exit()
+                cls.fish_count = 0
+                return True
+            return False
+
+        # Function to sell fish or exit program
+        def sell_fish():
+            time.sleep(random.uniform(.25, .5))
+
+            # Click backpack
+            m.click(random.randint(1058, 1065), random.randint(915, 925))
+            # Wait for green sell box
+            m.wait_for_color(977, 353, (84, 234, 52), 2)
+
+            # Make sure backpack is selected
+            if not pyautogui.pixelMatchesColor(600, 345, (72, 242, 255)):
+                m.click(600, 345)
+
+            #  If there are fish in backpack, sell
+            if pyautogui.pixel(506, 461) != (249, 248, 248):
+                # click sell button
+                x, y = random.randint(910, 980), random.randint(340, 350)
+                m.click(x, y)
+                m.wait_for_color(1056, 406, (119, 255, 100), 2)
+
+                # click sell all
+                x, y = random.randint(1050, 1150), random.randint(410, 430)
+                m.click(x, y)
+                m.wait_for_color(1055, 411, (119, 255, 100), 2)
+
+                # click final sell button
+                x, y = random.randint(995, 1045), random.randint(410, 430)
+                m.click(x, y)
+                m.wait_for_color(995, 1045, (255, 255, 255), 2)
+
+            # Close backpack
+            m.click(random.randint(1058, 1065), random.randint(915, 925))
+
+            time.sleep(random.uniform(.25, .5))
+            # Cast
+            m.click(960, 520)
+
+        def is_bag_full():
+            """Detects if the bag full message is up"""
+            s = m.screenshot(0)
+            color = (253, 0, 97)  # "Cannot Catch Fish Because Backpack is Full" message color
+            # Step every 2 pixels to save time
+            for x in range(500, 650, 2):
+                for y in range(645, 750, 2):
+                    if s.getpixel((x, y)) == color:
+                        return True
+            return False
+
+        def chest_ready():
+            color = (255, 255, 255)
+            s = m.screenshot(0)
+            for x in range(875, 1000, 5):
+                for y in range(500, 700, 5):
+                    if s.getpixel((x, y)) == color:
+                        print('chest ready')
+                        return True
+            print('chest NOT ready')
+            return False
+
+        # Main loop for opening chests
+        if OPEN_CHESTS:
+            # Quick and dirty open chests.
+            m.click(697, 831)
+            while True:
+                if chest_ready():
+                    # Hold E
+                    pyautogui.keyDown('e')
+                    time.sleep(2)
+                    pyautogui.keyUp('e')
+                    # Click buy
+                    m.click(607, 791)
+                    time.sleep(6)
+                    if pyautogui.pixelMatchesColor(594, 314, (0, 213, 255)):
+                        print('Ran out of money')
+                        m.click(1442, 326)
+                        exit()
+                time.sleep(.25)
+
+        # Main loop to fish after checking that fishing pole is selected
+        m.click(720, 569)  # Click Window
+        time.sleep(.5)
+        is_pole_selected()
+        while True:
+            # Cast
+            double_click_random_cast()
+
+            # Check for full inventory or sell fish if needed
+            if check_fish_count():
+                continue
+
+            # Wait for bubbles
+            if wait_for_air_bubbles(TIME_FOR_BUBBLES):
+                # Hook then reel in the fish
+                click_random_cast()
+                fish_counter = reel()
+
+class WindowsHelper:
+    """Roblox robot common functions."""
+
+    @classmethod
+    def __init__(cls):
+        # Script setup Mousekey
+        cls.mkey = MouseKey()
+        # Kills the whole process
+        cls.mkey.enable_failsafekill('ctrl+c')
+
+    def click(self, x, y):
+        try:
+            self.mkey.left_click_xy_natural(
+                x, y,
+                delay=.2,  # duration of the mouse click (down - up)
+                # min_variation=-3,  # a random value will be added to each pixel  - define the minimum here
+                max_variation=3,  # a random value will be added to each pixel  - define the maximum here
+                use_every=4,  # use every nth pixel
+                sleeptime=(0.005, 0.009),  # delay between each coordinate
+                percent=99,  # the lower, the straighter the mouse movement
+            )
+        except:
+            win32api.SetCursorPos((x, y))
+            time.sleep(random.uniform(0.001, 0.1))
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+            time.sleep(random.uniform(0.001, 0.1))
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+
+    def move(self, x, y):
+        try:
+            self.mkey.move_to_natural(
+                x, y,
+                # min_variation=-3,  # a random value will be added to each pixel  - define the minimum here
+                max_variation=3,  # a random value will be added to each pixel  - define the maximum here
+                use_every=4,  # use every nth pixel
+                sleeptime=(0.005, 0.009),  # delay between each coordinate
+                print_coords=True,  # console output
+                percent=90,  # the lower, the straighter the mouse movement
+            )
+        except:
+            win32api.SetCursorPos((x, y))
+
+    def screenshot(self, counter=0):
+        return pyautogui.screenshot()
+
+    def wait_for_color(self, x, y, color, timeout):
+        """Wait for color at a coordinate."""
+        now = time.time()
+        while time.time() - now < timeout:
+            if pyautogui.pixelMatchesColor(x, y, color):
+                time.sleep(.25)
+                return True
+        return False
+
+    def wait_for_no_color(self, x, y, color, timeout):
+        """Wait until there is not the specified color at coordinates"""
+        now = time.time()
+        while time.time() - now < timeout:
+            if not pyautogui.pixelMatchesColor(x, y, color):
+                time.sleep(.25)
+                return True
+        return False
+
+    def wait_for_color_in_area(self, x1, x2, y1, y2, color, timeout):
+        """Waits for color in a boxed area"""
+        now = time.time()
+        while time.time() - now < timeout:
+            s = pyautogui.screenshot()
+            for x in range(x1, x2, 1):
+                for y in range(y1, y2, 1):
+                    if s.getpixel((x, y)) == color:
+                        return True
+        # Return False at end of the loop
+        return False
+
+    def wait_for_no_color_in_area(self, x1, x2, y1, y2, color, timeout):
+        """Waits until color not detected in a specified boxed area"""
+        now = time.time()
+        while time.time() - now < timeout:
+            if not self.wait_for_color_in_area(x1, x2, y1, y2, color, .25):
+                return True
+        # Return False at end of the loop
+        return False
+
+
+class MacHelper:
+    """Roblox robot common functions."""
+
+    @classmethod
+    def __init__(cls):
+        cls.sct = mss()
+
+    def click(self, x, y):
+        try:
+            pyautogui.click(x, y)
+        except:
+            pyautogui.click(x, y)
+
+    def move(x, y):
+        try:
+            pyautogui.moveTo(x, y, duration=.2, tween=pyautogui.easeInOutQuad)
+        except:
+            pyautogui.moveTo(x, y, duration=.2, tween=pyautogui.easeInOutQuad)
+
+    def screenshot(self, counter=0):
+        # The monitor or screen part to capture
+        monitor = self.sct.monitors[1]  # or a region
+        # Grab the data
+        sct_img = self.sct.grab(monitor)
+        # Generate the PNG
+        img = self._convert_to_srgb(pil.Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX"))
+        # resize to 1680 x 1050
+        img = img.resize((1680, 1050))
+        # output = f"monitor-{counter}.png"
+        # img.save(output)
+        return img
+
+    def _convert_to_srgb(self, img):
+        '''Convert PIL image to sRGB color space (if possible)'''
+        icc = img.info.get('icc_profile', '')
+        if icc:
+            io_handle = io.BytesIO(icc)  # virtual file
+            src_profile = ImageCms.ImageCmsProfile(io_handle)
+            dst_profile = ImageCms.createProfile('sRGB')
+            img = ImageCms.profileToProfile(img, src_profile, dst_profile)
+        return img
+
+    def wait_for_color(self, x, y, color, timeout):
+        """Wait for color at a coordinate."""
+        now = time.time()
+        while time.time() - now < timeout:
+            s = self.screenshot()
+            if s.getpixel((x, y)) == color:
+                time.sleep(.25)
+                return True
+        return False
+
+    def wait_for_no_color(self, x, y, color, timeout):
+        """Wait until there is not the specified color at coordinates"""
+        now = time.time()
+        while time.time() - now < timeout:
+            s = self.screenshot()
+            if not s.getpixel((x, y)) == color:
+                time.sleep(.25)
+                return True
+        return False
+
+    def wait_for_color_in_area(self, x1, x2, y1, y2, color, timeout):
+        """Waits for color in a boxed area"""
+        now = time.time()
+        while time.time() - now < timeout:
+            s = self.screenshot()
+            for x in range(x1, x2, 1):
+                for y in range(y1, y2, 1):
+                    if s.getpixel((x, y)) == color:
+                        return True
+        # Return False at end of the loop
+        return False
+
+    def wait_for_no_color_in_area(self, x1, x2, y1, y2, color, timeout):
+        """Waits until color not detected in a specified boxed area"""
+        now = time.time()
+        while time.time() - now < timeout:
+            if not self.wait_for_color_in_area(x1, x2, y1, y2, color, .25):
+                return True
+        # Return False at end of the loop
+        return False
+
+
+if __name__ == '__main__':
+    Fish.main()
